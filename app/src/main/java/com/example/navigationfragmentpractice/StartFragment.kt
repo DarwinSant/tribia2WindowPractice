8fcceeb3134be45457a8e24e31874de3b8607ed4
Darwin Santana Enrique <darwi1983@gmail.com>
2019-09-20 14:15:37 -0400

    initial commit with binding in mainActivity-binding in fragment_start-working-button start working

--- app/src/main/java/com/example/navigationfragmentpractice/StartFragment.kt
+++ app/src/main/java/com/example/navigationfragmentpractice/StartFragment.kt
@@ -7,6 +7,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.navigationfragmentpractice.databinding.FragmentStartBinding
 
 
@@ -20,6 +22,10 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?    ): View? {
 
        val binding: FragmentStartBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_start,container,false)
+
+        binding.button.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_startFragment_to_directionFragment)
+        }
         return binding.root
 
     }
